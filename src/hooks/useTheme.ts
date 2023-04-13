f10616a7910af5f40aa143aafc7959314493bc82
Anna <anysofronova@yandex.ru>
2023-04-13 11:43:16 +0200

    feat: hooks

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,12 @@
+import { useLayoutEffect, useState } from 'react'
+
+const defaultTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+export const useTheme = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('app-theme') || defaultTheme)
+  useLayoutEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('app-theme', theme)
+  }, [theme])
+
+  return { theme, setTheme }
+}
