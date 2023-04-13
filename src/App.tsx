adc635939f4db7585741aa7986e46da4833bd63a
Anna <anysofronova@yandex.ru>
2023-04-13 11:39:30 +0200

    feat: vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-function App() {
+export const App: FC = () => {
+  const { i18n } = useTranslation()
+  const changeLanguage = (language: string): void => {
+    i18n.changeLanguage(language)
+  }
+  AOS.init()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        Vite
+    </>
+  )
 }
-
-export default App;
